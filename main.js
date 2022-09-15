096f32c56f15d43d2423918b18f4231ca618af84
Unpei2 <r.ho5@share.epsb.ca>
2022-09-15 14:12:01 -0600

    new and improved

--- main.js
+++ main.js
@@ -20,17 +20,7 @@ function allnames(){
 function randomname(){
     let first = document.getElementById("firstname").value
     let last = document.getElementById("lastname").value
-    let randomnumber = Math.random();
-    if (randomnumber <= 0.2){
-        asd = 0
-    }  else if (randomnumber <= 0.4){
-        asd = 1
-    }  else if (randomnumber <= 0.6){
-        asd = 2
-    }   else if (randomnumber <= 0.8){
-        asd = 3
-    }   else {
-        asd = 4
-    }
-dis.innerHTML = first + nicknames[asd] + last
+    let randomnumber =  Math.round(Math.random() * (4 - 0))
+    console.log(randomnumber)
+    dis.innerHTML = first + nicknames[randomnumber] + last
 }
